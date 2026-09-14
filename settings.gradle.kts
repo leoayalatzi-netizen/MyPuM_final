11ab936cb8027aa5055450e5f10f431aa3a4e056
leoayalatzi-netizen <leoayalatzi-netizen@users.noreply.github.com>
2026-09-14 09:47:19 -0600

    fix: eliminar doble definicion del version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,18 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
-    versionCatalogs { create("libs") { from(files("gradle/libs.versions.toml")) } }
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
+
 rootProject.name = "MyPuM"
 include(":app")
