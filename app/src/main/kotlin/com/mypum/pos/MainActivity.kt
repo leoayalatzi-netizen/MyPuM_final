70a298186afe9f634fd158d2a2415e9c446e81b2
leoayalatzi-netizen <leoayalatzi-netizen@users.noreply.github.com>
2026-09-14 23:17:35 -0600

    Fix MainActivity navigation controller

--- app/src/main/kotlin/com/mypum/pos/MainActivity.kt
+++ app/src/main/kotlin/com/mypum/pos/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.mypum.pos
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.fillMaxSize
-import com.mypum.pos.designsystem.theme.MyPuMTheme
-import com.mypum.pos.navigation.MyPuMNavHost
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import com.mypum.pos.navigation.MyPuMNavHost
+import com.mypum.pos.ui.theme.MyPuMTheme
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState); enableEdgeToEdge(); setContent { MyPuMTheme { Surface(Modifier.fillMaxSize()) { MyPuMNavHost() } } } }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MyPuMTheme {
+                val nav = rememberNavController()
+                MyPuMNavHost(nav = nav)
+            }
+        }
+    }
 }
